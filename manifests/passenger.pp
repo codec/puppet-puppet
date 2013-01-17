20b84fc476c6868acfbe82c0317a1620f73a543a
codec <codec@fnord.cx>
2013-01-17 16:58:51 +0100

    hackery: make passenger work properly on ubuntu
    Conflicts:
    
    	manifests/passenger.pp

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -11,34 +11,41 @@
 # Sample Usage:
 #
 class puppet::passenger {
-  include ruby::dev
-  include apache::ssl
-  include ::passenger
-  include passenger::params
-  include ::rack
+  case $::osfamily {
+    'Debian': {
+      include puppet::server::passenger::debian
+    }
 
-  $passenger_version=$passenger::params::version
-  $gem_path=$passenger::params::gem_path
+    default: {
+      include ruby::dev
+      include apache::ssl
+      include ::passenger
+      include passenger::params
+      include ::rack
+  
+      $passenger_version=$passenger::params::version
+      $gem_path=$passenger::params::gem_path
 
-  file {
-    ['/etc/puppet/rack', '/etc/puppet/rack/public', '/etc/puppet/rack/tmp']:
-      owner  => 'puppet',
-      group  => 'puppet',
-      ensure => directory,
-  }
-  file {
-    '/etc/puppet/rack/config.ru':
-      owner  => 'puppet',
-      group  => 'puppet',
-      mode   => '0644',
-      source => 'puppet:///modules/puppet/config.ru',
-  }
+      file { ['/etc/puppet/rack', '/etc/puppet/rack/public', '/etc/puppet/rack/tmp']:
+          owner  => 'puppet',
+          group  => 'puppet',
+          ensure => directory,
+      }
 
-  apache::vhost{ 'puppetmaster':
-    port     => '8140',
-    priority => '10',
-    docroot  => '/etc/puppet/rack/public/',
-    ssl      => true,
-    template => 'puppet/vhost/apache/passenger.conf.erb',
+      file { '/etc/puppet/rack/config.ru':
+        owner  => 'puppet',
+        group  => 'puppet',
+        mode   => '0644',
+        source => 'puppet:///modules/puppet/config.ru',
+      }
+
+      apache::vhost{ 'puppetmaster':
+        port     => '8140',
+        priority => '10',
+        docroot  => '/etc/puppet/rack/public/',
+        ssl      => true,
+        template => 'puppet/vhost/apache/passenger.conf.erb',
+      }
+    }
   }
 }
